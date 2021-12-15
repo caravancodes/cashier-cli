ff7a726bd1e5e0cd16b622d1a4023d36029dff7f
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-12-15 23:48:40 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+/*
+ * Created by faisalamir on 15/12/21
+ * cashier-cli
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2021 FrogoBox Inc.
+ * All rights reserved
+ *
+ */
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
